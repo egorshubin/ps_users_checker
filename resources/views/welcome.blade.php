9fca96872877d150903e3ad85b3712895780e3a9
George <egor.shubin@bk.ru>
2024-09-25 14:59:20 +0000

    Initial Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body {
+            margin: 0;
+        }
+        .screen {
+            height: 100vh;
+            width: 100%;
+            background-image: url("img/cherry-blossom.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="screen"></div>
+</body>
+</html>
